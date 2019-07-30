38cdf79f88b351866337bb21a9e9a8d336e6b444
technickal1 <31661206+technickal1@users.noreply.github.com>
2019-07-30 00:59:45 -0400

    Update simplyGoogle.js

--- simplyGoogle.js
+++ simplyGoogle.js
@@ -10,5 +10,14 @@ var modalLinkCSS = document.createElement("link");
     $(modalLinkCSS).attr('rel',"stylesheet");
     $(modalLinkCSS).attr('href','https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css');
     $("head").append(modalLinkCSS);
+
+document.write( '<div class=\"dropdown\">\n' );
+document.write( '  <button class=\"dropbtn\">Dropdown</button>\n' );
+document.write( '  <div class=\"dropdown-content\">\n' );
+document.write( '    <a href=\"#\">Link 1</a>\n' );
+document.write( '    <a href=\"#\">Link 2</a>\n' );
+document.write( '    <a href=\"#\">Link 3</a>\n' );
+document.write( '  </div>\n' );
+document.write( '</div>' );
     
     
