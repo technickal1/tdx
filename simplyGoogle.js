4a1dfee3cc2b75df7399295d8c909572f8a4395e
technickal1 <31661206+technickal1@users.noreply.github.com>
2019-07-30 01:19:48 -0400

    Update simplyGoogle.js

--- simplyGoogle.js
+++ simplyGoogle.js
@@ -11,36 +11,6 @@ var modalLinkCSS = document.createElement("link");
     $(modalLinkCSS).attr('href','https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css');
     $("head").append(modalLinkCSS);
 
-
-	var $hideFeed = $('<input id="btn-toggle" type="button" value="TOGGLE FEED" />');
-	$hideFeed.appendTo($("body")); // 2. Append somewhere
-
-	$($hideFeed).click(function() {
-		$("#divFeed").toggle();
-	});
-	
-	var $hideDetails = $('<input id="btn-details" type="button" value="TOGGLE DETAILS" />');
-	$hideDetails.appendTo($("body")); // 2. Append somewhere
-
-	$($hideDetails).click(function() {
-		$("#divDetails").toggle();
-	});
-
-  // HIDE AND SHOW BUTTONS
-	var $hideItem = $('<div class="hideItem"></div>').text("hide");
-	var $showItem = $('<div class="showItem"></div>').text("show");
-
-	$(".feed-item").before($hideItem); //prepend button
-	$(".feed-item").before($showItem); //prepend button
-
-	// $(".feed-item").click(function() {
-	// 	$(this).css("background-color","yellow");
-	// });
-
-	$(".hideItem").click(function() {
-		$(".feed-item", this).css("background-color","yellow");
-	});
-
 	
 
     
